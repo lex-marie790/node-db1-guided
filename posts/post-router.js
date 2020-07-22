4796e2d6febf840e97e11b4c2bcdb608b682195a
lex-marie790 <lexi.marie790@gmail.com>
2020-07-21 21:06:15 -0700

    class work

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,23 +5,71 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
+//api/posts
 router.get('/', (req, res) => {
-
+    //SELECT * FROM posts;
+    db.select('*')
+    .from('posts')
+    .then((postsArray) => res.status(200).json({ data:postsArray }))
+    .catch( err => console.log(err));
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    //SELECT * FROM posts WHERE id = id;
+    db('posts')
+    // .where("id", "=", id)
+    // .where({ id: 1});
+    .where('id',id)
+    .first()
+    .then(post => res.status(200).json({ data:post }))
+    .catch( err => console.log(err));
 });
 
 router.post('/', (req, res) => {
-
+    const postData = req.body;
+    // INSERT INTO posts (fields...) VALUES (values...)
+    db('posts')
+    .insert(postData)
+    .then(id => res.json(201).json({ data:id }))
+    c
 });
 
 router.put('/:id', (req, res) => {
-
+    const { id } = req.params;
+    const changes = req.body;
+    //UPDATE posts SET field = "new value" WHERE id = id;
+    db('posts')
+    .where('id', id)
+    .update(changes)
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({ data: count })
+        } else {
+            res.status(404).json({
+                message: 'there was no record updated'
+            })
+        }
+    })
+    .catch( err => console.log(err));
 });
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    //DELETE posts SET field = "new value" WHERE id = id;
+    db('posts')
+    .where('id', id)
+    .del()
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({ data: count })
+        } else {
+            res.status(404).json({
+                message: 'there was no record deleted'
+            })
+        }
+    })
+    .catch( err => console.log(err));
 
 });
 
